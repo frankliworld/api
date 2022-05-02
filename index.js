7ae73538b0636fb4b9236c037f4853d2ab9dd766
Frank Arias <32846100+frankliworld@users.noreply.github.com>
2022-05-02 16:16:19 -0500

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const user = require("./user.controller");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 mongoose.connect('mongodb+srv://space:O6GXmPqy24x9qHPq@cluster0.5pte8.mongodb.net/myApp?retryWrites=true&w=majority')
@@ -14,15 +14,19 @@ app.put("/users/:id", user.update)
 app.patch("/users/:id", user.update)
 app.delete("/users/:id", user.remove)
 
-app.use(express.static("app"));
+// app.use(express.static("app"));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  console.log(req.ip)
+  console.log(req.ips)
+  console.log(req.originalUrl)
+  res.send('<h1>Hello World!</h1>')
+  // res.sendFile(__dirname + "/index.html");
 })
 
 app.get("*", (req, res) => {
   res.status(404).send("Not Found");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
